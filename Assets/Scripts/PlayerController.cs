e14658ecff4c31ed941633d7ece5210cde1cb0b6
Luiza de Souza <souza.luiza@unifesp.br>
2025-02-08 22:37:32 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _playerRigidbody2D;
+    public  float       _playerSpeed;
+    private Vector2     _playerDirection;
+    
+    void Start()
+    {
+        _playerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _playerDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        _playerRigidbody2D.MovePosition(_playerRigidbody2D.position + _playerDirection * _playerSpeed * Time.fixedDeltaTime);
+    }
+}
