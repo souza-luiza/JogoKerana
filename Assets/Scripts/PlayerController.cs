dd0bed23443fed6b79bb1c9bd503c68313cccec4
Luiza de Souza <souza.luiza@unifesp.br>
2025-02-18 02:04:52 -0300

    Anim-Kerana/ Mosquinha-target

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,20 +3,42 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D _playerRigidbody2D;
+    private Animator    _playerAnimator;
     public  float       _playerSpeed;
     private Vector2     _playerDirection;
     
     void Start()
     {
         _playerRigidbody2D = GetComponent<Rigidbody2D>();
+        _playerAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        _playerDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        
     }
 
     void FixedUpdate()
+    {
+        _playerDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if(_playerDirection.sqrMagnitude > 0.1)
+        {
+            MovePlayer();
+
+            _playerAnimator.SetFloat("AxisX", _playerDirection.x);
+            _playerAnimator.SetFloat("AxisY", _playerDirection.y);
+
+            _playerAnimator.SetInteger("Movimento", 1);
+        }
+        else
+        {
+            _playerAnimator.SetInteger("Movimento", 0);
+        }
+        // if ataque verdadeiro, movimento = 2
+    }
+
+    void MovePlayer()
     {
         _playerRigidbody2D.MovePosition(_playerRigidbody2D.position + _playerDirection * _playerSpeed * Time.fixedDeltaTime);
     }
